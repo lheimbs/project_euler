084574f8d74102e5d3352ec2195478a0d278aba3
Lennart Heimbs <l.heimbs@gmail.com>
2021-04-13 07:45:39 +0200

    Add solution for problem 31

--- Python/031_coin_sums.py
+++ Python/031_coin_sums.py
@@ -1,6 +1,6 @@
 
-def combinations(iterable, goal, summ=0, combination=None):
-#    print(summ, combination, id(sum), id(combination))
+def combinations(iterable, goal, summ=0, combination=None, latest=None):
+#    print(summ, combination, iterable)
 
     if summ == goal:
         yield tuple(combination)
@@ -11,23 +11,25 @@ def combinations(iterable, goal, summ=0, combination=None):
         for elem in iterable:
             combination = [elem]
             s = elem
-            yield from combinations(iterable, goal, s, combination)
+            yield from combinations(iterable, goal, s, combination, elem)
     else:
+        if not iterable:
+            return
+        if latest is not None:
+            iterable = iterable[iterable.index(latest):]
+
         for elem in iterable:
             if summ + elem > goal:
                 continue
             s = summ + elem
             combi = combination[:]
             combi.append(elem)
-            yield from combinations(iterable, goal, s, combi)
+            yield from combinations(iterable, goal, s, combi, elem)
 
 
-summs = set()
 i = 0
 for c in combinations([1,2,5,10,20,50,100,200], 200):
-#    print(sum(c), c)#, end=", ")
     i += 1
-    summs.add(tuple(sorted(c)))
 
-print(len(summs), i)
+print(i)
 
